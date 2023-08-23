9cfb3ceb2ca451bf1751c8fb9a4195f45ff82c96
Bilal <bilalmirza079@gmail.com>
2023-08-23 14:41:28 +0500

    Provider St Mgt

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    print('sdkljf');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
